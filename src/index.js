3c5435bd99a01e97fa03fa3a75d7fb9122f7e0f5
shimagh74 <shima.ghasemzade92@gmail.com>
2024-07-22 21:23:02 +1000

    13

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 
 
 root.render(
   <React.StrictMode>
-    <HashRouter >
+    <BrowserRouter >
     <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
